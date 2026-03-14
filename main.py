fd1db88bdb300202826fc9ecdfa97795109e2ae0
NBhey <122431282+NBhey@users.noreply.github.com>
2026-03-14 19:40:00 +0300

    оразбор преременных и типов данных

---
+++ main.py
@@ -0,0 +1,32 @@
+name = 'User'
+print('Добыйр день ', name, ".", sep="")
+
+name_second="User 2"
+
+print(f"Are u mirin, {name_second}, bro?")
+
+print(f"{'What?':0>9}")
+print(f"{'What?':0<9}")
+print(f"{'What?':0^9}")
+print("Сложно" + "слов")
+print("Просто" + 'слов')
+print('test ' * 10)
+
+num_1 = 10
+num_2 = 2.3
+
+print(num_1 + num_2)
+
+num_3 = 12
+print(float(num_3)) #12.0
+
+print(int(num_2) + num_3)
+
+width = "3.7"
+height = '2.1'
+
+summa = float(width) + float(height)
+print(summa)
+print(4 / 2) #2.0
+print(f"{2 ** 0.5}") #1.4142135623730951
+print(f"{2 ** 0.5: .2f}") #1.41
