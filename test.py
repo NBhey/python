d2ef658cfb0fe6790bf461e81018206464778698
NBhey <122431282+NBhey@users.noreply.github.com>
2026-03-18 20:01:34 +0300

    операции с числами, строками

---
+++ test.py
@@ -0,0 +1,12 @@
+# print('Привет, Яндекс!')
+#
+# name = input('Как вас зовут? ')
+# print(f'Привет, {name}')
+#
+# info=input('Three time')
+#
+# print(info,info,info, sep='\n')
+
+number = input('Число ')
+summa = 38 * 2.5
+print(int(number) - summa )
