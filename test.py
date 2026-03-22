ae2faf39e291000e55c2210f420cdcb8a8f933f2
NBhey <122431282+NBhey@users.noreply.github.com>
2026-03-22 21:33:46 +0300

    check

--- test.py
+++ test.py
@@ -6,7 +6,28 @@
 # info=input('Three time')
 #
 # print(info,info,info, sep='\n')
+# print(f"{2 ** 0.5:.0f}")
+#
+# number = input('Число ')
+# summa = 38 * 2.5
+# print(int(int(number) - summa) )
 
-number = input('Число ')
-summa = 38 * 2.5
-print(int(number) - summa )
+# cost = input('цена товара за единицу веса:')
+# weight = input('вес товара: ')
+# money = input('количество денег у покупателя ')
+#
+# result = int(money) - int(cost)  * int(weight)
+# print(result)
+
+product = input()
+cost = input()
+weight = input()
+money = input()
+print('Чек')
+print(f'{product} - {weight}кг - {cost}руб/кг')
+print(f'Итого: {int(weight) * int(cost)}руб')
+print(f'Внесено: {money}руб')
+print(f'Сдача: {int(money) - int(weight) * int(cost)}руб')
+
+print(f"Чек\n{product} - {weight}кг - {cost}руб/кг\nИтого: {int(weight) * int(cost)}руб\nВнесено: {money}руб\n"
+      f"Сдача: {int(money) - int(weight) * int(cost)}руб")
