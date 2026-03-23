306d38e72406b7758ba823ee93d00f1a5a3f2fd0
NBhey <122431282+NBhey@users.noreply.github.com>
2026-03-23 21:14:27 +0300

    задачÐва J

--- test.py
+++ test.py
@@ -19,15 +19,38 @@
 # result = int(money) - int(cost)  * int(weight)
 # print(result)
 
-product = input()
-cost = input()
-weight = input()
-money = input()
-print('Чек')
-print(f'{product} - {weight}кг - {cost}руб/кг')
-print(f'Итого: {int(weight) * int(cost)}руб')
-print(f'Внесено: {money}руб')
-print(f'Сдача: {int(money) - int(weight) * int(cost)}руб')
+# product = input()
+# cost = input()
+# weight = input()
+# money = input()
+# print('Чек')
+# print(f'{product} - {weight}кг - {cost}руб/кг')
+# print(f'Итого: {int(weight) * int(cost)}руб')
+# print(f'Внесено: {money}руб')
+# print(f'Сдача: {int(money) - int(weight) * int(cost)}руб')
+#
+# print(f"Чек\n{product} - {weight}кг - {cost}руб/кг\nИтого: {int(weight) * int(cost)}руб\nВнесено: {money}руб\n"
+#       f"Сдача: {int(money) - int(weight) * int(cost)}руб")
 
-print(f"Чек\n{product} - {weight}кг - {cost}руб/кг\nИтого: {int(weight) * int(cost)}руб\nВнесено: {money}руб\n"
-      f"Сдача: {int(money) - int(weight) * int(cost)}руб")
+# repeat = int(input())
+# print('Купи слона!\n' * repeat)
+
+# repeat = int(input())
+# text = input()
+#
+# print(f'Я больше никогда не буду писать "{text}"!\n' * repeat)
+#
+# children = int(input())
+# time = int(input())
+#
+# print((children // 2) * time)
+
+name = input()
+number = int(input())
+
+group = number // 100
+bed = ((number % 100) - number % 10) // 10
+number_children = number % 10
+
+print(f"Группа №{group}.\n{number_children}. {name}.\nШкафчик: {number}.\nКроватка: {bed}.")
+
