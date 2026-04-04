cce5bc4e64ac34aa4e68a23f876b3d86fa8e4ef7
NBhey <122431282+NBhey@users.noreply.github.com>
2026-04-04 22:21:09 +0300

    список победителей

--- conditional.py
+++ conditional.py
@@ -28,3 +28,28 @@
 #     print('Петя')
 # else:
 #     print('Вася')
+
+petya = int(input())
+vasya = int(input())
+tolya = int(input())
+
+if petya > vasya and petya > tolya:
+    print('1. Петя')
+elif vasya > petya and vasya > tolya:
+    print('1. Вася')
+else:
+    print('1. Толя')
+
+if (petya > vasya and petya < tolya) or (petya > tolya and petya < vasya):
+    print('2. Петя')
+elif (petya < vasya and vasya < tolya) or (petya > vasya and vasya > tolya):
+    print('2. Вася')
+else:
+    print('2. Толя')
+
+if petya < vasya and petya < tolya:
+    print('3. Петя')
+elif vasya < petya and vasya < tolya:
+    print('3. Вася')
+else:
+    print('3. Толя')
