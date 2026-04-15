be93bdd9ab084e9592beeb3d47ec0fb373cf8126
NBhey <122431282+NBhey@users.noreply.github.com>
2026-04-15 18:22:23 +0300

    Ðзайка - 3

--- cycle.py
+++ cycle.py
@@ -1,3 +1,10 @@
-while (row := input()) != 'Три!':
-    print('Режим ожидания...')
-print('Ёлочка, гори!')
+# while (row := input()) != 'Три!':
+#     print('Режим ожидания...')
+# print('Ёлочка, гори!')
+
+count = 0
+
+while (text := input()) != 'Приехали!':
+    if 'зайка' in text:
+        count += 1
+print(count)
