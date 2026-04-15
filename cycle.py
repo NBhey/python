75889f7b3e421c9bc70e1e3b2cdede4defed7343
NBhey <122431282+NBhey@users.noreply.github.com>
2026-04-15 18:26:49 +0300

    ÑсчиталочкаÑ

--- cycle.py
+++ cycle.py
@@ -2,9 +2,15 @@
 #     print('Режим ожидания...')
 # print('Ёлочка, гори!')
 
-count = 0
+# count = 0
+#
+# while (text := input()) != 'Приехали!':
+#     if 'зайка' in text:
+#         count += 1
+# print(count)
 
-while (text := input()) != 'Приехали!':
-    if 'зайка' in text:
-        count += 1
-print(count)
+num_1 = int(input())
+num_2 = int(input())
+
+for num in range(num_1, num_2 + 1):
+    print(num, end=' ')
