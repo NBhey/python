db32d40e21f9d5d3261e9fbbc63545832035c427
NBhey <122431282+NBhey@users.noreply.github.com>
2026-04-23 21:22:34 +0300

    новогоднее настроение

--- innerCycle.py
+++ innerCycle.py
@@ -5,8 +5,21 @@
 #         print(j * i, end=" ")
 #     print(sep='\n')
 
-size = int(input())
+# size = int(input())
+#
+# for i in range(1, size + 1):
+#     for j in range(1, size + 1):
+#         print(f'{j} * {i} = {i * j}')
 
-for i in range(1, size + 1):
-    for j in range(1, size + 1):
-        print(f'{j} * {i} = {i * j}')
+limit = int(input())
+
+counter = 0
+for i in range(1, limit + 1):
+    for j in range(counter, counter + i):
+        counter += 1
+        print(counter, end=" ")
+        if counter == limit:
+            break
+    if counter == limit:
+        break
+    print()
