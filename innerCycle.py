d8242e4d0c0316980ee2491ce97aedbbbfa60a35
NBhey <122431282+NBhey@users.noreply.github.com>
2026-04-28 22:16:14 +0300

    зайка-5

--- innerCycle.py
+++ innerCycle.py
@@ -24,12 +24,29 @@
 #         break
 #     print()
 
-number = int(input())
-total = 0
-for i in range(1, number + 1):
-    user_number = int(input())
-    while user_number > 0:
-        total += user_number % 10
-        user_number //= 10
+# number = int(input())
+# total = 0
+# for i in range(1, number + 1):
+#     user_number = int(input())
+#     while user_number > 0:
+#         total += user_number % 10
+#         user_number //= 10
+#
+# print(total)
 
-print(total)
+repeat_cycle = int(input())
+repeat_counter = 0
+
+rabbit_in_area = True
+rabbit_counter = 0
+
+while repeat_counter != repeat_cycle:
+    area = input()
+    if area == 'зайка' and rabbit_in_area:
+        rabbit_in_area = False
+        rabbit_counter += 1
+    if area == 'ВСЁ':
+        rabbit_in_area = True
+        repeat_counter += 1
+
+print(rabbit_counter)
